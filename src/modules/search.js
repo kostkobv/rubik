b22faefb17e363333ad6b036635a74c141effe8f
Bohdan Kostko <bohdan.kostko@gmail.com>
2016-12-10 23:39:14 +0100

    feat(search): pagination

--- src/modules/search.js
+++ src/modules/search.js
@@ -7,13 +7,15 @@ import requests from './services/requests';
  */
 function SearchModule(config) {
   this.config = config;
+  this.articles = [];
+  this.page = 0;
 }
 
 /**
  * Fetches the articles
  * @returns {Promise} - result of get articles
  */
-SearchModule.prototype.get = function () {
+SearchModule.prototype.fetch = function () {
   return requests()
     .get(this.config.get.endpoint, this.config.get.params)
     .then(res => this.parseArticles(res));
@@ -25,7 +27,75 @@ SearchModule.prototype.get = function () {
  * @returns {Array} - articles
  */
 SearchModule.prototype.parseArticles = function (res) {
-  return res.body.articles;
+  this.articles = res.body.articles;
+
+  return this.articles;
+};
+
+/**
+ * Returns the start and end indexes for stack slice.
+ * If page number is less than 0 it will return indexes for first page
+ *
+ * @param {Number} pageNumber - desired page number
+ * @param {Number} articlesPerPage - articles per page
+ * @returns {[{Number}, {Number}]} - start and end index for slicing the stack
+ * to get content for desired page
+ */
+function getPageIndex(pageNumber, articlesPerPage) {
+  const FIRST_PAGE_INDEX = 0;
+  let nextPageNumber = pageNumber;
+
+  if (pageNumber < FIRST_PAGE_INDEX) {
+    nextPageNumber = FIRST_PAGE_INDEX;
+  }
+
+  const startIndex = nextPageNumber * articlesPerPage;
+  const endIndex = startIndex + articlesPerPage;
+
+  return [
+    startIndex,
+    endIndex
+  ];
+}
+
+/**
+ * Returns the stack with recently fetched articles
+ * @param {Number} pageNumber - page for which articles should be returned
+ * @returns {Array} - articles
+ */
+SearchModule.prototype.getArticles = function (pageNumber = 0) {
+  const indexes = getPageIndex(pageNumber, this.config.articlesPerPage);
+  const nextPageArticles = this.articles.slice(...indexes);
+
+  if (nextPageArticles.length) {
+    // update the actual page number
+    this.page = pageNumber;
+  }
+
+  return nextPageArticles;
+};
+
+/**
+ * Automatically goes to the next page
+ *
+ * @returns {Array} - next page articles
+ */
+SearchModule.prototype.nextPage = function () {
+  const nextPageNumber = this.page + 1;
+
+  return this.getArticles(nextPageNumber);
+};
+
+/**
+ * Automatically goes to the previous page.
+ * If no previous page returns articles for first page
+ *
+ * @returns {Array} - previous page articles
+ */
+SearchModule.prototype.previousPage = function () {
+  const previousPageNumber = this.page - 1;
+
+  return this.getArticles(previousPageNumber);
 };
 
 export default function init(config) {
--- test/modules/search.js
+++ test/modules/search.js
@@ -13,6 +13,13 @@ describe('search module', () => {
     searchInstance = search(config);
   });
 
+  beforeEach(() =>
+    nock
+      .get(config.get.endpoint)
+      .query(config.get.params)
+      .reply(200, articlesMock)
+  );
+
   it('should create new instance each time', () => {
     const testConfig = { test: 'test' };
     const newSearchInstance = search(testConfig);
@@ -21,12 +28,96 @@ describe('search module', () => {
     return expect(newSearchInstance).not.eql(searchInstance).and.contain(testConfig);
   });
 
-  it('should be able to retrieve all articles', () => {
-    nock
-      .get(config.get.endpoint)
-      .query(config.get.params)
-      .reply(200, articlesMock);
+  it('should be able to retrieve all articles', () =>
+    searchInstance.fetch().then(articles => expect(articles.length).to.equal(9))
+  );
 
-    return searchInstance.get().then(articles => expect(articles.length).to.equal(9));
+  it('should keep all of the articles in stack', () =>
+    searchInstance.fetch().then(() =>
+      expect(searchInstance.articles.length).to.equal(9)
+    )
+  );
+
+  describe('pagination', () => {
+    it('should be able of return the articles by pages', () => {
+      searchInstance.fetch().then(() =>
+        expect(searchInstance.getArticles(1).length).to.equal(4)
+      );
+    });
+
+    it('should be able to go to the next page', () => {
+      searchInstance.fetch().then(() => {
+        searchInstance.getArticles(0);
+
+        return expect(searchInstance.nextPage().length).to.equal(4);
+      });
+    });
+
+    it('should be able to go to the next page and update the actual page number', () => {
+      searchInstance.fetch().then(() => {
+        searchInstance.getArticles(0);
+        searchInstance.nextPage();
+
+        return expect(searchInstance.page).to.equal(1);
+      });
+    });
+
+    it('should return empty array if there is no next page', () => {
+      searchInstance.fetch().then(() => {
+        searchInstance.getArticles(1);
+
+        return expect(searchInstance.nextPage()).to.be.empty;
+      });
+    });
+
+    it('should not increment page number if there is no next page', () => {
+      searchInstance.fetch().then(() => {
+        searchInstance.getArticles(1);
+
+        const pageNumber = searchInstance.page;
+
+        searchInstance.nextPage();
+
+        return expect(searchInstance.page).to.be.equal(pageNumber);
+      });
+    });
+
+    it('should be able to go to the previous page', () => {
+      searchInstance.fetch().then(() => {
+        searchInstance.getArticles(1);
+
+        return expect(searchInstance.previousPage().length).to.be.equal(5);
+      });
+    });
+
+    it('should decrement page number if there is previous page', () => {
+      searchInstance.fetch().then(() => {
+        const pageNumber = searchInstance.page;
+
+        searchInstance.getArticles(pageNumber + 1);
+        searchInstance.previousPage();
+
+        return expect(searchInstance.page).to.be.equal(pageNumber);
+      });
+    });
+
+    it('should return first page if there is no previous page', () => {
+      searchInstance.fetch().then(() => {
+        searchInstance.getArticles();
+
+        return expect(searchInstance.previousPage().length).to.be.equal(5);
+      });
+    });
+
+    it('should not go to previous page if it is on a first page', () => {
+      searchInstance.fetch().then(() => {
+        const pageNumber = searchInstance.page;
+        searchInstance.getArticles();
+
+        searchInstance.previousPage();
+
+        return expect(searchInstance.page).to.be.equal(pageNumber);
+      });
+    });
   });
 });
